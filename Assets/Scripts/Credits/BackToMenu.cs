b8cf7fac40b1c32ab34408c9965e0b50c548f6f0
RyzAnugrah <asep.anugerah50@gmail.com>
2021-04-09 01:51:26 +0700

    add main menu + option + credit

---
+++ Assets/Scripts/Credits/BackToMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+
+    [SerializeField]
+    private string sceneMenuToLoad;
+
+    private float timeElapsed;
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneMenuToLoad);
+        }
+    }
+}
